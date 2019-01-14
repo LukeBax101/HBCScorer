724714bb5c944ed52d60e39728b86dcf7addbb33
Luke Baxter <lbb29@cam.ac.uk>
2019-01-14 23:56:05 +0000

    Init Commit

---
+++ router-app/src/components/Welcome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Welcome">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Welcome',
+    data () {
+      return {
+        msg: 'Welcome to HBC'
+      }
+    }
+  }
+</script>
